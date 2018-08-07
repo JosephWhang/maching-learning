ee88c28d8c24987ddd690d5de8e681dd7530f50f
JosephWhang <615418349@qq.com>
2018-08-07 16:58:21 +0800

    change 6

--- HandWrittingPy/Test.py
+++ HandWrittingPy/Test.py
@@ -1,8 +1,15 @@
 from numpy import *
 import matplotlib.pyplot as plt
+import tensorflow as tf
 
 group = array([[1.0,1.1],[1.0,1.0],[0.0,0.0],[0.0,0.1]])
 print(group[:,0],group[:,1])
 
 a = 3**2
 print(a)
+
+x = tf.placeholder("float", shape=[None, 784])
+print(x)
+
+W = tf.Variable(tf.zeros([784, 10]))
+print(W)
